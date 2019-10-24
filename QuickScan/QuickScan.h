b2f41897c3cd9e0a9122a40c884f865aee982fcc
Aly Yakan <a.yakan@newmotion.com>
2019-10-24 14:05:45 +0200

    Initial commit

---
+++ QuickScan/QuickScan.h
@@ -0,0 +1,19 @@
+//
+//  QuickScan.h
+//  QuickScan
+//
+//  Created by Aly Yakan on 24/10/2019.
+//  Copyright © 2019 Aly Yakan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QuickScan.
+FOUNDATION_EXPORT double QuickScanVersionNumber;
+
+//! Project version string for QuickScan.
+FOUNDATION_EXPORT const unsigned char QuickScanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuickScan/PublicHeader.h>
+
+
